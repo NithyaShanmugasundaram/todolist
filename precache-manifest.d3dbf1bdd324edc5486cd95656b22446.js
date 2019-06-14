77aa670d0cf781056f8753eb294c28db14d95691
Ganesh kumar M <gunz@Ganeshs-MacBook-Pro.local>
2019-06-14 17:27:24 +0800

    Updates

---
+++ precache-manifest.d3dbf1bdd324edc5486cd95656b22446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5da5275e5db3e453eb5a930581866a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "193fcb4b3cfcfcd2db6c",
+    "url": "/todolist/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/todolist/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "193fcb4b3cfcfcd2db6c",
+    "url": "/todolist/static/js/main.1120c875.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
